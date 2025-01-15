a0a2c1add24d32f4a9c953bcc3bd50c7c643600a
maetheartist <mariamkosemani1@gmail.com>
2025-01-15 10:51:39 +0100

    using JavaScript ES6 functions to refresh memory

---
+++ index.js
@@ -0,0 +1,37 @@
+// refresher practice
+
+const heading = document.querySelector("h1");
+heading.style.color = "teal";
+heading.style.fontSize= "70px";
+
+// SCOPE
+// global and block scope, for example the variable heading I defined above is in a global scope and can be used anywhere on this page while block scope is within the confines of a function
+
+// LET: This is used in declaring variables which may change later, it is loosely typed and used before ES6 was introduced,and therefore no longer recommended so as to avoid errors. for example:
+
+let myName = "Ade";
+myName = "Joseph"
+console.log(myName);
+
+// CONST: This variable declaration on the other is more srictly typed as opposed to let, once const has been used, you can't redeclare such variable, it will throw an error message, for example:
+
+const userName = "Bash";
+// const userName = "Yahya";
+console.log(userName); 
+// error: Uncaught SyntaxError: Identifier 'userName' has already been declared (at index.js:16:7)
+
+// Array and Array methods
+
+// the array below contains many data types like string, number, boolean, and also a nested array. Map and Filter are also known as high order functions.
+const myArray = [1, "Bola,", 90, ["Bread,", "Eggs", "Milo", "Lipton"], true, "Ava", "London", "Parrot", "Ruler", 140, false];
+
+// mapping the array
+const mappedArr = myArray.map((item)=>{
+    console.log(item);
+})
+
+// filtering the array
+const filteredArr = myArray.filter((item)=> {
+  return  item.length === 4
+})
+console.log(filteredArr);
